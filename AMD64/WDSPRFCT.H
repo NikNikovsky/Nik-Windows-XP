4350f945140ee72de1343f943d5c9383f4957583
Nik Nikovsky <75254052+NikNikovsky@users.noreply.github.com>
2023-03-05 01:41:41 +0100

    Add files via upload
    Signed-off-by: Nik Nikovsky <75254052+NikNikovsky@users.noreply.github.com>

---
+++ AMD64/WDSPRFCT.H
@@ -0,0 +1,24 @@
+/*++
+
+    Copyright (c) 2005 Microsoft Corporation
+
+Module Name:
+
+    WdsPrfCtr.h
+
+Abstract:
+
+    This module defines Performance Counters for WDS Server.
+
+Environment:
+
+    User Mode
+
+--*/
+
+#define     WdsPertCtr_Requests_Per_Sec                     0
+#define     WdsPerfCtr_Responses_Per_sec                    2
+#define     WdsPerfCtr_Active                               4
+
+#define     WdsPerfCtr_Object                               6
+
